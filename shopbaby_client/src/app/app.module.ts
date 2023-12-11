5ae5530bbc77ed5965ab845e838bb62f3ff1eb5e
VINGROUP\binhtt28 <v.binhtt28@vinfast.vn>
2023-12-11 17:21:22 +0700

    update

--- shopbaby_client/src/app/app.module.ts
+++ shopbaby_client/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
+import { OrderconfirmComponent } from './orderconfirm/orderconfirm.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    OrderComponent
+    OrderComponent,
+    OrderconfirmComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
   ],
   providers: [],
-  bootstrap: [OrderComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
