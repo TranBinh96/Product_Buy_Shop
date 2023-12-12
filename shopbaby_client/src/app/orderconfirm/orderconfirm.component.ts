eea2d818df576b2466c72480558e9d299f630f5c
binhtt28 <v.binhtt28@vinfast.cn>
2023-12-12 08:03:06 +0700

    fdgf

---
+++ shopbaby_client/src/app/orderconfirm/orderconfirm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orderconfirm',
+  templateUrl: './orderconfirm.component.html',
+  styleUrls: ['./orderconfirm.component.scss']
+})
+export class OrderconfirmComponent {
+
+}
